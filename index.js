f296d2e880908f581979cae400df7f220a5576b2
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-10-04 15:07:20 -0700

    imported db functions to index

--- index.js
+++ index.js
@@ -5,22 +5,7 @@ const server = express();
 
 server.use(express.json());
 
-const users = [
-    {
-        id: 1,
-        name: 'Alice',
-        bio: 'An andevtures girl'
-    },
-    {
-        id: 2,
-        name: 'Bob',
-        bio: 'An adventures boy'
-    }
-]
-
-const generateId = () => {
-    return users.length ? Math.max(...users.map(user => user.id)) + 1 : 0;
-}
+const {find, findById, insert, update, remove} = require('./data/db')
 
 server.get('/', (req, res) => {
     res.send('Hello World!');
@@ -40,50 +25,63 @@ server.get('/hobbits', (req, res) => {
     res.status(200).json(hobbits);
 })
 
-server.post('/api/users', (req, res) => {
+server.post('/api/users', async (req, res) => {
     const user = req.body;
     if(!user.name || !user.bio){
         res.status(400).json({message: 'Please provide name and bio for the user.'})
     } else {
-        user.id = generateId();
-        users.push(user);
-        res.status(201).json(user);
+      try {
+        const newUser = await insert(user);
+        res.status(201).json(newUser)
+      } catch (error){
+        res.status(500).json({message: 'There was an error while saving the user to the database'})
+      }
     }
 });
 
-server.get('/api/users/:id', (req, res) => {
+server.get('/api/users/:id', async (req, res) => {
     const id = parseInt(req.params.id);
-    const user = users.find(user => user.id === id);
-    if (user){
-        res.status(200).json(user);
-    } else {
-        res.status(404).json({message: 'The user with the specified ID does not exist.'})
+    try {
+        const foundUser = await findById(id);
+        if(foundUser){
+            res.status(200).json(foundUser);
+        } else {
+            res.status(404).json({message: 'The user with the specified ID does not exist.'})
+        }
+    } catch (error){
+        res.status(500).json({message: 'The user information could not be retrieved'})
     }
 })
 
-server.delete('/api/users/:id', (req, res) => {
+server.delete('/api/users/:id', async (req, res) => {
     const id = parseInt(req.params.id);
-    const index = users.findIndex(user => user.id === id);
-    if (index !== -1){
-        const deletedUser = users.splice(index, 1)[0];
-        res.status(200).json(deletedUser);
-    } else {
-        res.status(404).json({message: 'The user with specified ID does not exist.'})
+    try {
+        const deletedUser = await remove(id);
+        if(deletedUser){
+            res.status(200).json(deletedUser);
+        } else {
+            res.status(404).json({message: 'The user with specified ID does not exist.'})
+        }
+    } catch (error){
+        res.status(500).json({message: 'The user could not be removed'})
     }
 })
 
-server.put('/api/users/:id', (req, res) => {
+server.put('/api/users/:id', async (req, res) => {
     const id = parseInt(req.params.id);
     const updatedUser = req.body;
     if(!updatedUser.name || !updatedUser.bio){ 
         res.status(400).json({message: 'Please provide name and bio for the user.'})
     } else {
-        const index = users.findIndex(user => user.id === id);
-        if(index !== -1){
-            users[index] = {...updatedUser, id};
-            res.status(200).json(users[index]);
-        } else {
-            res.status(404).json({message: 'The user with the specified ID does not exist'});
+        try {
+            const updateCount = await update(id, updatedUser);
+            if(updateCount){
+                res.status(200).json(updateCount);
+            } else {
+                res.status(404).json({message: 'The user with the specified ID does not exist'})
+            }
+        } catch (error){
+            res.status(500).json({message: 'The user information could not be modified.'})
         }
     }
 });
